ea26d433b978e6f0e83fb0fab63e119bff15a771
hariomenkel <mario.henkel4@gmail.com>
2019-06-19 10:13:19 +0200

    Tidy up code
    Make it less worse

--- azo_decrypt.py
+++ azo_decrypt.py
@@ -7,100 +7,117 @@ output_file = ""
 
 ''' This function removes the part of the POST request which is not part of the payload itsself '''
 def extract_payload(data):
-	# We are searching for a specific byte sequence which marks the start of our payload
-	found = data.find('\x0d\x0a\x0d\x0a')
-	payload = data[found + 4:]
-	return data[found + 4:]
-	
+    # We are searching for a specific byte sequence which marks the start of our payload
+    found = data.find('\x0d\x0a\x0d\x0a')
+    payload = data[found + 4:]
+    return data[found + 4:]
+
+
+''' Inside the payload there is a ZIP file containint text files with stolen credentials, cookies etc. so we extract it'''
 def ExtractZipFiles(startpos, data):
-	zip_begin = "\x50\x4b\x03\x04"
-	zip_end = "\x50\x4b\x05\x06"
-	start = data.find(zip_begin)
-	print "Found ZIP begin @ " + str(start)
-	end = data.find(zip_end)
-	print "Found ZIP end @ " + str(end)
-	
-	zip_file = data[start:end]
-	
-	file = open(str(start) + "_" + str(end) + ".zip", "wb")
-	file.write(zip_file)
-	file.close
-	
-	
-	
+    zip_begin = "\x50\x4b\x03\x04"
+    zip_end = "\x50\x4b\x05\x06"
+    start = data.find(zip_begin)
+    print "Found ZIP begin @ " + str(start)
+    end = data.find(zip_end)
+    print "Found ZIP end @ " + str(end)
+
+    zip_file = data[start:end]
+
+    file = open(str(start) + "_" + str(end) + ".zip", "wb")
+    file.write(zip_file)
+    file.close
+
 
 ''' This function was found in index.php of AzoRult panel which does the decryption of the paylod '''
-''' Since we want to be able to manipuate the content we first have to decrypt it '''
 def CB_XORm(data, key, max):
-	datalen=len(data);
-	keylen=len(key);
-	if (datalen >= max):
-		datalen=max;
-		
-	j = 0;
-	i = 0;
-	
-	while i < datalen:
-		data[i] = chr(data[i]^ord(key[j]));
-		j = j + 1;
-		if( j > (keylen-1)):
-			j=0;
-		i = i + 1;
-	return data;
-	
+    datalen = len(data);
+    keylen = len(key);
+    if (datalen >= max):
+        datalen = max;
+
+    j = 0;
+    i = 0;
+
+    while i < datalen:
+        data[i] = chr(data[i] ^ ord(key[j]));
+        j = j + 1;
+        if (j > (keylen - 1)):
+            j = 0;
+        i = i + 1;
+    return data;
+
+
+''' Self explanatory I guess '''
 def WriteToDisk(value, filename):
-	file = open(filename, "w")
-	file.write(value)
-	file.close
-	print "File was written to disk: " + filename
+    file = open(filename, "w")
+    file.write(value)
+    file.close
+    print "File was written to disk: " + filename
+
 
 def decrypt():
-	xor_key = chr(13) + chr(10) + chr(200)
-	print "<Key>" + xor_key + "</Key>"
+    # This key was found in index.php and is used to decrypt the content
+    # Since this key is also hard coded into the client it is highly unlikely that this will ever change
+    # since no one will put the effort into changing the client I guess
+    xor_key = chr(13) + chr(10) + chr(200)
+    print "<Key>" + xor_key + "</Key>"
 
-	input_byte = bytearray(open(input_file, 'rb').read())
+    input_byte = bytearray(open(input_file, 'rb').read())
 
-	data_sanitized = extract_payload(input_byte)	
+    # We strip the POST data from the payload
+    data_sanitized = extract_payload(input_byte)
+    size = len(data_sanitized)
 
-	size = len(data_sanitized)
+    # Finally we decrypt the data using the XOR key
+    decrypted = CB_XORm(data_sanitized, xor_key, 1024 * 512)
 
-	decrypted = CB_XORm(data_sanitized, xor_key, 1024*512)	
+    # The payload either starts with "G" which contains just a unique system id...
+    if "G" in str(decrypted)[0]:
+        print "Content started with G"
+        substring = str(decrypted)[1:]
+        payload = unquote(substring)
+        WriteToDisk(payload, output_file)
+        ExtractZipFiles(str(decrypted)[1:])
 
-	if "G" in str(decrypted)[0]:
-		print "Content started with G"
-		substring = str(decrypted)[1:]
-		payload = unquote(substring)
-		WriteToDisk(payload, output_file)
-		ExtractZipFiles(str(decrypted)[1:])
-		
-	elif "<" in str(decrypted)[0]:
-		print "Content started with <"
-		substring = str(decrypted)
-		payload = unquote(substring)
-		WriteToDisk(payload, output_file)
-		print "Extracting ZIP file(s)"
-		ExtractZipFiles(0, decrypted)
-	
+    # ... or it starts with "<" which indicates that this POST contains the stolen data
+    elif "<" in str(decrypted)[0]:
+        print "Content started with <"
+        substring = str(decrypted)
+        payload = unquote(substring)
+        WriteToDisk(payload, output_file)
+        print "Extracting ZIP file(s)"
+        ExtractZipFiles(0, decrypted)
+
+
+def print_header():
+    print "                    _____                             _"
+    print "    /\             |  __ \                           | |"
+    print "   /  \    _______ | |  | | ___  ___ _ __ _   _ _ __ | |_"
+    print "  / /\ \  |_  / _ \| |  | |/ _ \/ __| '__| | | | '_ \| __|"
+    print " / ____ \  / / (_) | |__| |  __/ (__| |  | |_| | |_) | |_"
+    print "/_/    \_\/___\___/|_____/ \___|\___|_|   \__, | .__/ \__|"
+    print "                                         __/ | |"
+    print "                                        |___/|_|"
+    print ""
+
+
 if len(sys.argv) < 3:
-	print "AzoDecrypt"
-	print "**********"
-	print ""
-	print "Usage: " + sys.argv[0] + " <POST Data File> <Output File>"
+    print_header()
+    print "Usage: " + sys.argv[0] + " <POST Data File> <Output File>"
 else:
-	print "AzoDecrypt"
-	print "**********"
-	print ""
-	input_file = sys.argv[1]
-	output_file = sys.argv[2]
-	
-	if os.path.isfile(input_file):
-		print "Using file '" + input_file + "' as input."
-		print "Writing output to '" + output_file + "'"
-		decrypt()
-	else:
-		print "[!] Couldn't find input file; Aborting."
-	
-	
-	
-	
+    print_header()
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if os.path.isfile(input_file):
+        print "Using file '" + input_file + "' as input."
+        print "Writing output to '" + output_file + "'"
+        decrypt()
+    else:
+        print "[!] Couldn't find input file; Aborting."
+
+
+
+
 
